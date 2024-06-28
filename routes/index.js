af9bcb9a0850b5f44dee32b2e1c9844fd39eddad
Violet Stanziani <stanzianiviolet@gmail.com>
2024-06-28 11:45:55 -0400

    imported router

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = require('express').Router();
 const tipsRouter = require('./tips');
 const feedbackRouter = require('./feedback');
 // TODO: import your diagnostics route
+const diagnosticsRouter = require('./diagnostics')
 
 router.use('/tips', tipsRouter);
 router.use('/feedback', feedbackRouter);
+router.use('/diagnostics', diagnosticsRouter);
 // TODO: Initialize diagnostics route
 
 module.exports = router;
