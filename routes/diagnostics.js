c9e338db8a69a9ceaf95350f7aa83e305f89e531
Violet Stanziani <stanzianiviolet@gmail.com>
2024-06-28 11:43:55 -0400

    wrote diagnostics code

--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -5,11 +5,26 @@ const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 // GET Route for retrieving diagnostic information
 diagnostics.get('/', (req, res) => {
   // TODO: Logic for sending all the content of db/diagnostics.json
+  readFromFile('./db/diagnostics.json').then((data)=> res.json(JSON.parse(data)));
 });
 
 // POST Route for a error logging
 diagnostics.post('/', (req, res) => {
   // TODO: Logic for appending data to the db/diagnostics.json file
+  console.log(req.body)
+  
+  const { time, error_id, errors} = req.body 
+  if (req.body) {
+    const newDiagnostic = {
+      time,
+      errors_id: uuidv4(),
+      errors,
+      }
+      readAndAppend(newDiagnostic, './db/diagnostics.json');
+      res.json('Diagnostic result added successfully')
+  } else {
+    res.error('Error in adding diagnostic results')
+  }
 });
 
 module.exports = diagnostics;
